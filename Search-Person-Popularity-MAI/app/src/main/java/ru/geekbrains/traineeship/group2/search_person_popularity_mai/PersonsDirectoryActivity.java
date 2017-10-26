7d4c02a0903feb587de93b7a09776e9e683fb7b5
skubatko <skubatko@gmail.com>
2017-10-26 23:02:40 +0300

    Создан графический интерфейс начального экрана, сделана связка с остальными Activities

--- Search-Person-Popularity-MAI/app/src/main/java/ru/geekbrains/traineeship/group2/search_person_popularity_mai/PersonsDirectoryActivity.java
+++ Search-Person-Popularity-MAI/app/src/main/java/ru/geekbrains/traineeship/group2/search_person_popularity_mai/PersonsDirectoryActivity.java
@@ -1,13 +1,31 @@
 package ru.geekbrains.traineeship.group2.search_person_popularity_mai;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class PersonsDirectoryActivity extends AppCompatActivity {
+public class PersonsDirectoryActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_persons_directory);
+
+        final Button btnBackPersonsDirectory = (Button) findViewById(R.id.btnBackPersonsDirectory);
+        btnBackPersonsDirectory.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch ( v.getId() ){
+            case R.id.btnBackPersonsDirectory:
+                Intent intent = new Intent();
+                setResult(RESULT_OK,intent);
+                finish();
+                break;
+        }
     }
 }
--- Search-Person-Popularity-MAI/app/src/main/java/ru/geekbrains/traineeship/group2/search_person_popularity_mai/SitesDirectoryActivity.java
+++ Search-Person-Popularity-MAI/app/src/main/java/ru/geekbrains/traineeship/group2/search_person_popularity_mai/SitesDirectoryActivity.java
@@ -1,13 +1,32 @@
 package ru.geekbrains.traineeship.group2.search_person_popularity_mai;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SitesDirectoryActivity extends AppCompatActivity {
+public class SitesDirectoryActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sites_directory);
+
+        final Button btnBackSitesDirectory = (Button) findViewById(R.id.btnBackSitesDirectory);
+        btnBackSitesDirectory.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch ( v.getId() ){
+            case R.id.btnBackSitesDirectory:
+                Intent intent = new Intent();
+                setResult(RESULT_OK,intent);
+                finish();
+                break;
+        }
+
     }
 }
